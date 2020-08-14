67947be71e415291895bb11b20a77925d3ff68eb
YannickSimo <s.asyncrite@gmail.com>
2020-08-14 13:47:30 +0200

    First Version 1.0

---
+++ Horloge/digital clock/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace digital_clock
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Content = DateTime.Now.ToString("HH:mm");
+            lblSecond.Content = DateTime.Now.ToString("ss");
+            lblDays.Content = DateTime.Now.ToString("dddd");
+            lblYear.Content = DateTime.Now.ToString("MMM dd yyyy");
+        }
+
+       
+    }
+}
